75ddecd9a03fe8afa172f095a2b52696e0526072
Devansh <devansh28112004@gmail.com>
2024-11-30 08:57:42 +0530

    multer fixed (storaged destination was not defined and renamed the resume

--- routes/extraRoutes.js
+++ routes/extraRoutes.js
@@ -7,7 +7,24 @@ import { safeHandler } from '../middlewares/safeHandler.js';
 const router = express.Router();
 
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../uploads/resumes'));
+    },
+    filename: (req, file, cb) => {
+        const userName = req.body.candidateName || "unknown_user"; //don't forget to add candidateName in the request body 
+        const currentDate = new Date().toISOString().slice(0, 10); 
+        const fileExtension = path.extname(file.originalname);
 
-router.get('/parse', upload.single("resume") ,safeHandler(async (req, res) => {
-    console.log(req.file);
-}));
+        const customFileName = `${userName}_resume_${currentDate}${fileExtension}`;
+        cb(null, customFileName);
+    }
+});
+
+const upload = multer({ storage });
+
+router.get("/parse",upload.single("resume"),safeHandler(async (req, res) => {    // resume here is the name of the form 
+        console.log(req.file);
+        res.send({ message: "File uploaded successfully", file: req.file });
+    })
+);
