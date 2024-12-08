cf731d0bcecd32a36d3676a08075886ee2707abc
Parth Katiyar <parthdev0101@gmail.com>
2024-12-08 15:32:26 +0530

    Add route log for debugging

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const app = express();
 
 connectMongo();
 //checkout populate or whatevr
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    console.log(req.url, req.method);
+    next();
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
